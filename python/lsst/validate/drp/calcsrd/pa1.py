c6c21896c45defc95229458296954bc2994a34e5
Jonathan Sick <jsick@lsst.org>
2016-06-23 16:49:22 -0700

    Create PA1 measurement class (first of new kind)

---
+++ python/lsst/validate/drp/calcsrd/pa1.py
@@ -0,0 +1,133 @@
+# LSST Data Management System
+# Copyright 2008-2016 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <https://www.lsstcorp.org/LegalNotices/>.
+
+from __future__ import print_function, absolute_import
+
+import operator
+import numpy as np
+
+import lsst.pipe.base as pipeBase
+
+from ..base import MeasurementBase, MetricBase, SpecLevel
+from ..util import getRandomDiffRmsInMas, computeWidths
+
+
+class PA1Metric(MetricBase):
+    """Definitional object of the PA1 metric, defined in LPM-17."""
+
+    name = 'PA1'
+    reference = 'LPM-17'
+    description = 'Median RMS of visit-to-visit relative photometry.'
+    operator = operator.lt
+    specs = {'design': SpecLevel(5., 'millimag'),
+             'minimum': SpecLevel(8., 'millimag'),
+             'stretch': SpecLevel(3., 'millimag')}
+
+
+class PA1Measurement(MeasurementBase):
+    """Measurement of the PA1 metric: photometric repeatability of
+    measurements across a set of observations.
+
+    Parameters
+    ----------
+    matchedDataset : lsst.validate.drp.matchreduce.MatchedMultiVisitDataset
+    numRandomShuffles : int
+        Number of times to draw random pairs from the different observations.
+    verbose : bool, optional
+        Output additional information on the analysis steps.
+    linkedBlobs : list, optional
+        A `list` of additional blobs (subclasses of BlobSerializerBase) that
+        can provide additional context to the measurement, though aren't
+        direct dependencies of the computation (e.g., `matchedDataset).
+
+    Notes
+    -----
+    We calculate differences for 50 different random realizations of the
+    measurement pairs, to provide some estimate of the uncertainty on our RMS
+    estimates due to the random shuffling.  This estimate could be stated and
+    calculated from a more formally derived motivation but in practice 50
+    should be sufficient.
+
+    The LSST Science Requirements Document (LPM-17), or SRD, characterizes the
+    photometric repeatability by putting a requirement on the median RMS of
+    measurements of non-variable bright stars.  This quantity is PA1, with a
+    design, minimum, and stretch goals of (5, 8, 3) millimag following LPM-17
+    as of 2011-07-06, available at http://ls.st/LPM-17.
+
+    This present routine calculates this quantity in two different ways:
+
+    1. RMS
+    2. interquartile range (IQR)
+
+    and also returns additional quantities of interest:
+
+    - the pair differences of observations of stars,
+    - the mean magnitude of each star
+
+    While the SRD specifies that we should just compute the RMS directly, the
+    current filter doesn't screen out variable stars as carefully as the SRD
+    specifies, so using a more robust estimator like the IQR allows us to
+    reject some outliers.  However, the IRQ is also less sensitive some
+    realistic sources of scatter such as bad zero points, that the metric
+    should include.
+    """
+
+    metric = None
+    value = None
+    units = 'millimag'
+    label = 'PA1'
+    schema = 'pa1-1.0.0'
+
+    def __init__(self, matchedDataset, numRandomShuffles=50, verbose=False,
+                 linkedBlobs=None):
+        MeasurementBase.__init__(self)
+        self.metric = PA1Metric()
+
+        # register input parameters for serialization
+        # note that matchedDataset is treated as a blob, separately
+        self.registerParameter('num_random_shuffles', numRandomShuffles)
+
+        self.matchedDataset = matchedDataset
+
+        # Add external blob so that links will be persisted with
+        # the measurement
+        if linkedBlobs is not None:
+            for blob in linkedBlobs:
+                self.linkBlob(blob)
+        self.linkBlob(self.matchedDataset)
+
+        matches = matchedDataset.safeMatches
+        magKey = matchedDataset.magKey
+        pa1Samples = [self._calc_PA1_sample(matches, magKey)
+                      for n in range(numRandomShuffles)]
+
+        self.rmsPA1 = np.array([pa1.rms for pa1 in pa1Samples])
+        self.iqrPA1 = np.array([pa1.iqr for pa1 in pa1Samples])
+
+        self.value = np.mean(self.iqrPA1)
+
+    def _calc_PA1_sample(self, groupView, magKey):
+        magDiffs = groupView.aggregate(getRandomDiffRmsInMas, field=magKey)
+        magMean = groupView.aggregate(np.mean, field=magKey)
+        rmsPA1, iqrPA1 = computeWidths(magDiffs)
+        return pipeBase.Struct(rms=rmsPA1, iqr=iqrPA1,
+                               rmsUnits='mmag', iqrUnits='mmag',
+                               magDiffs=magDiffs, magMean=magMean,
+                               magDiffsUnits='mmag', magMeanUnits='mag')
